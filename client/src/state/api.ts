fc3f70d247b9c4fb200de9c44bd80ef881ce3f20
S2T Candidate <jobs@s2t.ai>
2023-07-21 18:04:49 +0800

    changed env retrieval and build to public dir

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from './types';
 
+
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.VITE_BASE_URL }),
     reducerPath: "main",
     tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({

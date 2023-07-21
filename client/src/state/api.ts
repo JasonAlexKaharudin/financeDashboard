d5c1808ca528d3f65793c315cb844f3a23dfd6b5
S2T Candidate <jobs@s2t.ai>
2023-07-21 18:09:31 +0800

    hardcoded backend URL

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -3,7 +3,7 @@ import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from '.
 
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.VITE_BASE_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://empty-morning-9725.fly.dev" }),
     reducerPath: "main",
     tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
